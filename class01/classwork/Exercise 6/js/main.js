6927006ee8c85fe328891afda93eb1b321ca0425
HannaAugust <hannaleibscher@gmail.com>
2016-09-21 03:31:04 -0500

    Update main.js

--- class01/classwork/Exercise 6/js/main.js
+++ class01/classwork/Exercise 6/js/main.js
@@ -2,29 +2,54 @@
 //
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Working_with_Objects
-
 function jsonFlickrApi (data) {
-	//console.log(data);
-	//console.log(data.photos.photo.length);
-	//var length = data.photos.photo.length;
+	console.log(data);
 
+
+var gallery = document.getElementsByClassName('gallery')[0];
 	data.photos.photo.forEach(function(photo) {
-		document.getElementsByClassName('gallery').appendChild('a');
-		console.log(photo.url_s);
-		console.log(photo.url_l);
-      	console.log(photo.title);
-      	console.log(photo.description.content);
-      	console.log(photo.datetaken);
-      	console.log(photo.ownername);
-      	console.log("www.flickr.com/"+photo.owner);
+		var figcap = document.createElement("figcaption");
+    	figcap.innerHTML = photo.title;
+    	var figure = document.createElement("figure");
+		gallery.appendChild(figure);
+		figure.appendChild(figcap);
+		var a = document.createElement("a");
+		figure.appendChild(a);
+		var href = document.createAttribute("href");       
+		href.value = photo.url_l;                           
+		a.setAttributeNode(href);
+		var img = document.createElement('img');
+		a.appendChild(img);
+		var src = document.createAttribute('src');
+		src.value = photo.url_s;
+		img.setAttributeNode(src);
+
+		var description = document.createElement('p');
+		description.innerHTML = photo.description._content;
+		figure.appendChild(description);
+
+		var date = document.createElement('p');
+		date.innerHTML = "Date taken: " + photo.datetaken;
+		figure.appendChild(date);
+
+		var userlink = document.createElement('a');
+		
+		userlink.innerHTML = "Username: " + photo.ownername;
+		var userhref = document.createAttribute("href");
+		var target = document.createAttribute("target");
+		userhref.value = 'https://www.flickr.com/photos/' + photo.owner;
+		target.value = '_blank';
+		userlink.setAttributeNode(userhref);
+		userlink.setAttributeNode(target);
+		figure.appendChild(userlink);
+
+		// console.log(photo.url_s);
+		// console.log(photo.url_l);
+  //    	console.log(photo.title);
+  //    	console.log(photo.description.content);
+  //    	console.log(photo.datetaken);
+  //    	console.log(photo.ownername);
+  //    	console.log("www.flickr.com/"+photo.owner);
    });
 
-// var length = photo.length;
-// console.log(length);
-	// properties
-	// append title
-	// append image that links to bigger image
-	// append description
-	// append date taken
-	// append the user name along with a link to their flickr account
 }
