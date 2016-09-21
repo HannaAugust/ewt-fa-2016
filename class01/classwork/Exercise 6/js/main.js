7353e0620f9423f71c9f7de205fb8a89417cf94f
HannaAugust <hannaleibscher@gmail.com>
2016-09-20 19:26:01 -0500

    Update main.js

--- class01/classwork/Exercise 6/js/main.js
+++ class01/classwork/Exercise 6/js/main.js
@@ -1,23 +1,25 @@
-// The following function needs to be defined in order to receive data
-// from the Flickr API request we are making in line 12 in index.html
-// This is called a "JSONP" request --> https://en.wikipedia.org/wiki/JSONP
-
-// If you look at the console of your browser when you load the index
-// you will notice an object with properties of "photos" and "stat".
-
-// Inside of the function below you need to loop through data.photos.photo
-// since that is the array. You will have access to indivial photo objects
-// which you can then append via jQuery (or just plain JS) the properties
-// listed below to the ".gallery" div.
-
 // If you need to review loops and objects:
 //
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Working_with_Objects
 
 function jsonFlickrApi (data) {
-	console.log(data);
+	//console.log(data);
+	//console.log(data.photos.photo.length);
+	//var length = data.photos.photo.length;
 
+	data.photos.photo.forEach(function(photo) {
+		console.log(photo.url_s);
+		console.log(photo.url_l);
+      	console.log(photo.title);
+      	console.log(photo.description.content);
+      	console.log(photo.datetaken);
+      	console.log(photo.ownername);
+      	console.log("www.flickr.com/"+photo.owner);
+   });
+
+// var length = photo.length;
+// console.log(length);
 	// properties
 	// append title
 	// append image that links to bigger image
